fd80bc42b40b8f2ef2a6247f2b350fe445bb8f03
Wilton Moreira de Santana Júnior <wsantana3@gmail.com>
2023-07-14 01:31:55 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('home'), name='home'),
     path('admin/', admin.site.urls),
     path('perfil/', include('perfil.urls')),
     path('extrato/', include('extrato.urls')),
